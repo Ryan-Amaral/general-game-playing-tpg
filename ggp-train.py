954862eef71759a358704c5a8fa6b6067dea96b0
Ryan Amaral <ryan_amaral@live.com>
2018-08-01 17:56:11 -0300

    small improvements added

--- ggp-train.py
+++ ggp-train.py
@@ -10,6 +10,7 @@ import time
 import random
 import psutil
 import os
+import pickle
 
 
 """
@@ -17,23 +18,28 @@ inState is (row x col x rgba) list. This converts it to a 1-D list. Because
 that is what TPG uses.
 """
 def getState(inState):
-    outState = []
-    for row in inState:
-        for cell in row:
-            outState.append(cell[0]/8 + cell[1]*4 + cell[2]*128)
+    outState = [0]*int((len(inState)/5)*(len(inState[0])/5))
+    cnt = 0
+    for row in range(0, len(inState), 5):
+        for col in range(0, len(inState[row]), 5):
+            outState[cnt] = ((inState[row][col][0] >> 1) 
+                          + (inState[row][col][1] >> 2) 
+                          + (inState[row][col][2] >> 3))
+            cnt += 1
     
     return outState
 
 """
 Run each agent in this method for parallization.
 Args:
-    args: (TpgAgent, envName, scoreList, numEpisodes)
+    args: (TpgAgent, envName, scoreList, numEpisodes, numFrames)
 """
 def runAgent(args):
     agent = args[0]
     envName = args[1]
     scoreList = args[2]
     numEpisodes = args[3] # number of times to repeat game
+    numFrames = args[4] 
     
     # skip if task already done by agent
     if agent.taskDone(envName):
@@ -48,7 +54,16 @@ def runAgent(args):
     for ep in range(numEpisodes): # episode loop
         state = env.reset()
         scoreEp = 0
-        for i in range(1000): # frame loop
+        numRandFrames = 0
+        if numEpisodes > 1:
+            numRandFrames = random.randint(0,30)
+        for i in range(numFrames): # frame loop
+            if i < numRandFrames:
+                _, _, isDone, _ = env.step(env.action_space.sample())
+                if isDone: # don't count it if lose on random steps
+                    ep -= 1
+                continue
+
             act = agent.act(getState(state), valActs=valActs)
 
             # feedback from env
@@ -62,13 +77,13 @@ def runAgent(args):
         scoreTotal += scoreEp
         
     env.close()
-    agent.reward(scoreTotal, envName)
+    agent.reward(scoreTotal, envName+'-'+str(numEpisodes)+'-'+str(numFrames))
     scoreList.append((agent.getUid(), agent.getOutcomes()))
     
 # https://stackoverflow.com/questions/42103367/limit-total-cpu-usage-in-python-multiprocessing/42130713
 def limit_cpu():
     p = psutil.Process(os.getpid())
-    p.nice(19)
+    p.nice(10)
     
 # all of the titles we will be general game playing on
 gymEnvNames = ['Alien-v0','Asteroids-v0','Atlantis-v0','BankHeist-v0',
@@ -81,42 +96,57 @@ gymEnvNames = ['Alien-v0','Asteroids-v0','Atlantis-v0','BankHeist-v0',
                'UpNDown-v0','Venture-v0','WizardOfWor-v0','Zaxxon-v0']
 
 
-trainer = TpgTrainer(actions=range(18))
+trainer = TpgTrainer(actions=range(18), teamPopSizeInit=360)
 
-processes = 5
+processes = 2
 pool = mp.Pool(processes=processes, initializer=limit_cpu)
 man = mp.Manager()
 
-curEnvs = []
+curEnvNames = []
 numActiveEnvs = 10
 
-numEpisodes = 5
+numEpisodes = 0 # repeat evaluations to deal with randomness
+numFrames = 250 # number of frames per episode, to increase as time goes on
+
+allScores = [] # track all scores each generation
+
+tStart = time.time()
 
 while True: # do generations with no end
     scoreList = man.list()
     
     # reload curGames if needed
-    if len(curEnvs) == 0:
-        curEnvs = list(gymEnvNames)
-        random.shuffle(curEnvs)
-        curEnvs = curEnvs[:numActiveEnvs]
+    if len(curEnvNames) == 0:
+        curEnvNames = list(gymEnvNames)
+        random.shuffle(curEnvNames)
+        curEnvNames = curEnvNames[:numActiveEnvs]
+        # gradually increase the number of episodes and frames
+        if numEpisodes < 5:
+            numEpisodes += 1
+            numFrames += 150
         
-    curEnv = curEnvs.pop() # get env to play on this generation
+    curEnvName = curEnvNames.pop() # get env to play on this generation
     
     pool.map(runAgent, 
-        [(agent, curEnv, scoreList, numEpisodes)
+        [(agent, curEnvName, scoreList, numEpisodes, numFrames)
         for agent in trainer.getAllAgents(skipTasks=[])])
     
     # apply scores
     trainer.applyScores(scoreList)
-    trainer.evolve() # go into next gen
+    scoreStats = trainer.generateScoreStats(tasks=[curEnvName+'-'+str(numEpisodes)+'-'+str(numFrames)])
+    allScores.append((curEnvName, scoreStats['min'], scoreStats['max'], scoreStats['average']))
+
+    trainer.evolve(tasks=[curEnvName+'-'+str(numEpisodes)+'-'+str(numFrames)]) # go into next gen
     
     # save model after every gen
     with open('saved-model-1.pkl','wb') as f:
         pickle.dump(trainer,f)
         
-    clear_output(wait=True)
+    print(chr(27) + "[2J")
     print('Time Taken (Seconds): ' + str(time.time() - tStart))
-    print('On Generation: ' + trainer.curGen)
-    print('Results so far: ' + 
-          str(trainer.generateScoreStats(tasks=['curEnv'])))
+    print('On Generation: ' + str(trainer.curGen))
+    print('Results: ', str(allScores))
+
+
+
+
