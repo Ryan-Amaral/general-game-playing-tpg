e305941b7af41f52414002bf80524dff9ad80677
Ryan Amaral <ryan_amaral@live.com>
2019-02-26 01:32:54 -0400

    fixed errors

--- ggp-train.py
+++ ggp-train.py
@@ -11,6 +11,8 @@ from optparse import OptionParser
 from tpg_helpers import *
 import random
 import datetime
+import multiprocessing as mp
+import pickle
 
 """
 Command line arguments.
@@ -28,41 +30,8 @@ parser.add_option('-t', '--type', type='int', dest='trainType', default=0) # met
 (options, args) = parser.parse_args()
 
 """
-Create and initialize log files.
-"""
-
-timeStamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M"))
-
-# game score summary of each game in each generation
-logFileGameScoresName = 'ggp-log-gamescores-' + timeStamp + '.txt'
-if options.envGen == 0:
-    with open(logFileGenName, 'a') as f:
-        f.write('tpgGen,envName,tpgMin,tpgMax,tpgAvg,eliteSize,eliteUid\n')
-
-# every few generations evaluate top overall few teams on all titles
-logFileChampionsName = 'ggp-log-champions-' + timeStamp + '.txt'
-if options.envGen == 0:
-    with open(logFileMpName, 'a') as f:
-        f.write('tpgGen,teamId')
-        for envName in allEnvNames:
-            f.write(',score' + envName)
-        for envName in allEnvNames:
-            f.write(',vis' + envName)
-        f.write(',visTotal\n')
-
-# population fitness summary each generation
-logFileFitnessName = 'ggp-log-fitness-' + options.timeStamp + '.txt'
-if options.envGen == 0:
-    with open(logFileFitName, 'a') as f:
-        f.write('tpgGen,envs,fitMin,fitMax,fitAvg,champSize,champUid,totalTeams,totalRootTeams\n')
-
-# and create file to save TPG
-trainerFileName = 'tpg-trainer-' + options.timeStamp + '.pkl'
-
-"""
 General setup for GGP.
 """
-
 
 # all of the titles we will be general game playing on
 # we chose games that we know TPG does at-least OK in alone
@@ -78,24 +47,55 @@ allEnvNames = ['Alien-v0','Asteroids-v0','Atlantis-v0','BankHeist-v0',
 # shuffle if random and take certain number of games
 if options.randomGames:
     random.shuffle(allEnvNames)
-allEnvNames = allEnvNames[:numGames]
+allEnvNames = allEnvNames[:options.numGames]
+
+print('All Games: ' + str(allEnvNames))
 
 pool = mp.Pool(processes=options.workers, initializer=limit_cpu, maxtasksperchild=5)
 man = mp.Manager() # manager for shared memory lists
 
 """
+Create and initialize log files.
+"""
+
+timeStamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
+
+# game score summary of each game in each generation
+logFileGameScoresName = 'ggp-log-gamescores-' + timeStamp + '.txt'
+with open(logFileGameScoresName, 'a') as f:
+    f.write('tpgGen,envName,tpgMin,tpgMax,tpgAvg,eliteSize,eliteUid\n')
+
+# every few generations evaluate top overall few teams on all titles
+logFileChampionsName = 'ggp-log-champions-' + timeStamp + '.txt'
+with open(logFileChampionsName, 'a') as f:
+    f.write('tpgGen,teamId')
+    for envName in allEnvNames:
+        f.write(',score' + envName)
+    for envName in allEnvNames:
+        f.write(',vis' + envName)
+    f.write(',visTotal\n')
+
+# population fitness summary each generation
+logFileFitnessName = 'ggp-log-fitness-' + timeStamp + '.txt'
+with open(logFileFitnessName, 'a') as f:
+    f.write('tpgGen,envs,fitMin,fitMax,fitAvg,champSize,champUid,totalTeams,totalRootTeams\n')
+
+# and create file to save TPG
+trainerFileName = 'tpg-trainer-' + timeStamp + '.pkl'
+
+"""
 Method for training TPG on all games at once. Each individual in the single population
 will see all of the games before evolution will occur.
 """
-def ggpTrainAllAtOnce(envNames, popSize, lfGSName, lfCName, lfFName, trainerFileName, pool, man,
-        trainFrames=18000, testFrames=18000, trainEpisodes=3, evalEpisodes=10):
+def ggpTrainAllAtOnce(envNames, popSize, lfGSName, lfCName, lfFName, trainerFileName,
+        pool, man, trainFrames=18000, testFrames=18000, trainEpisodes=1, evalEpisodes=10):
     # create TPG
     trainer = TpgTrainer(actions=range(18), teamPopSize=popSize, maxProgramSize=128)
 
     envNamesSrt = sorted(list(envNames)) # for reporting envs played
 
     while True: # train indefinately
-        print('TPG Gen: ' + str(trainer.curGen))
+        print('TPG Gen: ' + str(trainer.populations[None].curGen))
         random.shuffle(envNames) # I don't think this actually matters
         for envName in envNames: # train on each env
             print('Playing Game: ' + envName)
@@ -111,16 +111,15 @@ def ggpTrainAllAtOnce(envNames, popSize, lfGSName, lfCName, lfFName, trainerFile
 
             # report curEnv results to log
             scoreStats = trainer.getTaskScores(envName)
+            bestTeam = trainer.getBestTeams(tasks=[envName])[0]
             with open(lfGSName, 'a') as f:
-                f.write(str(trainer.curGen) + ','
+                f.write(str(trainer.populations[None].curGen) + ','
                     + envName + ','
                     + str(scoreStats['min']) + ','
                     + str(scoreStats['max']) + ','
                     + str(scoreStats['average']) +  ','
-                    + str(len(trainer.getBestAgents(tasks=[envName],
-                                amount=1,topn=1)[0].team.getRootTeamGraph()[0])) + ','
-                    + str(trainer.getBestAgents(tasks=[envName],
-                                amount=1,topn=1)[0].getUid()) + '\n')
+                    + str(len(bestTeam.getRootTeamGraph()[0])) + ','
+                    + str(bestTeam.uid) + '\n')
 
         # do evolution after all envs played
         trainer.evolve(fitMthd='combine', tasks=envNames, elitistTasks=envNames)
@@ -128,23 +127,24 @@ def ggpTrainAllAtOnce(envNames, popSize, lfGSName, lfCName, lfFName, trainerFile
         # report generational fitness results
         bestTeam = trainer.getBestTeams(tasks=[envName])[0]
         with open(lfFName, 'a') as f:
-            f.write(str(trainer.curGen) + ','
+            f.write(str(trainer.populations[None].curGen) + ','
                 + '/'.join(envNamesSrt) + ','
-                + str(trainer.scoreStats['min']) + ','
-                + str(trainer.scoreStats['max']) + ','
-                + str(trainer.scoreStats['average']) +  ','
+                + str(trainer.populations[None].scoreStats['min']) + ','
+                + str(trainer.populations[None].scoreStats['max']) + ','
+                + str(trainer.populations[None].scoreStats['average']) +  ','
                 + str(len(bestTeam.getRootTeamGraph()[0])) + ','
-                + str(bestTeam.getUid()) + ','
-                + str(len(trainer.teams)) + ','
-                + str(len(trainer.rootTeams)) + '\n')
+                + str(bestTeam.uid) + ','
+                + str(len(trainer.populations[None].teams)) + ','
+                + str(len(trainer.populations[None].rootTeams)) + '\n')
 
         # save model after every gen
-        with open(trainerFile,'wb') as f:
-            pickle.dump(trainer,f)
+        # change trainer populations to classes instead of lambda to fix
+        #with open(trainerFileName,'wb') as f:
+        #    pickle.dump(trainer,f)
 
         # every 50 generations evaluate top agents on all games
-        if trainer.curGen % 50 == 0:
-            champEval()
+        #if trainer.populations[None].curGen % 50 == 0:
+        #    champEval()
 
 
 """
@@ -170,7 +170,7 @@ Run the training unending.
 
 if options.trainType == 0:
     ggpTrainAllAtOnce(allEnvNames, options.popSize, logFileGameScoresName,
-        logFileChampionsName, logFileFitnessName, pool, man)
+        logFileChampionsName, logFileFitnessName, trainerFileName, pool, man)
 elif options.trainType == 1:
     ggpTrainMerge(allEnvNames, options.popSize, options.popSizeMax, logFileGameScoresName,
         logFileChampionsName, logFileFitnessName, pool, man)
